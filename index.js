c2b15cdc8fae4e5ca20ecccf4e755eed032a3a2a
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-12-07 22:14:03 +0900

    Add volume support

--- index.js
+++ index.js
@@ -16,20 +16,35 @@ module.exports = (homebridge) => {
 class RegzaAccessory {
   constructor(log, config) {
     this._log = log;
+    this.name = config.name && "REGZA";
     this.host = config.host;
     this.user = config.user;
     this.pass = config.pass;
 
     this._service = new Service.Television("REGZA");
+    this._service.setCharacteristic(Characteristic.ConfiguredName, this.name);
+    this._service.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
     this._service.getCharacteristic(Characteristic.Active)
       .on('get', this._getActive.bind(this))
       .on('set', this._setActive.bind(this));
-    // this._speakerService = new Service.TelevisionSpeaker("REGZA Speaker");
-    // this._service.getCharacteristic(Characteristic.Mute)
-    //   .on('get', this._getMute.bind(this))
-    //   .on('set', this._setMute.bind(this));
-    // this._service.getCharacteristic(Characteristic.RemoteKey)
-    //   .on('set', this._setRemoteKey.bind(this));
+    this._service
+      .getCharacteristic(Characteristic.ActiveIdentifier)
+      .on("set", this._setActiveIdentifier.bind(this));
+    this._service
+      .getCharacteristic(Characteristic.RemoteKey)
+      .on("set", this._setRemoteKey.bind(this));
+
+    this._speakerService = new Service.TelevisionSpeaker("REGZA Volume", "REGZA Speaker");
+    this._speakerService.setCharacteristic(Characteristic.Active, Characteristic.Active.ACTIVE);
+    this._speakerService.setCharacteristic(Characteristic.VolumeControlType, Characteristic.VolumeControlType.RELATIVE);
+    // Not sure how to access this from iOS
+    this._speakerService.getCharacteristic(Characteristic.Mute)
+      .on('get', this._getMute.bind(this))
+      .on('set', this._setMute.bind(this));
+    this._speakerService
+      .getCharacteristic(Characteristic.VolumeSelector)
+      .on('set', this._setVolume.bind(this));
+    this._service.addLinkedService(this._speakerService);
   }
 
   getServices() {
@@ -38,7 +53,7 @@ class RegzaAccessory {
       .setCharacteristic(Characteristic.Manufacturer, "TOSHIBA")
       .setCharacteristic(Characteristic.Model, "Z720X");
 
-    return [informationService, this._service];
+    return [informationService, this._service, this._speakerService];
   }
 
   async _getActive(callback) {
@@ -58,21 +73,53 @@ class RegzaAccessory {
     callback();
   }
 
-  // async _getMute(callback) {
-  //   callback(null, await this._getMuteStatus());
-  // }
-  //
-  // async _setMute(on, callback) {
-  //   const desiredState = Boolean(on);
-  //   this._log("Setting switch to " + desiredState);
-  //   await this._sendKey('40BF10');
-  //   callback();
-  // }
-  //
-  // async _setRemoteKey(key, callback) {
-  //   this._log("Remote key: " + key);
-  //   callback();
-  // }
+  async _setActiveIdentifier(id, callback) {
+    this._log("Set active ", id);
+    callback();
+    ///await this._sendKey('40BF10');
+  }
+
+  async _getMute(callback) {
+    this._log("Disabling mute");
+    callback(null, true);
+  }
+
+  async _setMute(on, callback) {
+    this._log("Enabling mute");
+    const desiredState = Boolean(on);
+    await this._sendKey('40BF10');
+  }
+
+  async _setVolume(down, callback) {
+    this._log(`Setting volume: ${down ? 'down' : 'up'}`);
+    await this._sendKey(down ? '40BF1E' : '40BF1A');
+    callback();
+  }
+
+  async _setRemoteKey(key, callback) {
+    const map = {
+      [Characteristic.RemoteKey.REWIND]: '',
+      [Characteristic.RemoteKey.FAST_FORWARD]: '',
+      [Characteristic.RemoteKey.NEXT_TRACK]: '',
+      [Characteristic.RemoteKey.PREVIOUS_TRACK]: '',
+      [Characteristic.RemoteKey.ARROW_UP]: '40BF3E',
+      [Characteristic.RemoteKey.ARROW_DOWN]: '40BF3F',
+      [Characteristic.RemoteKey.ARROW_LEFT]: '40BF5F',
+      [Characteristic.RemoteKey.ARROW_RIGHT]: '40BF5B',
+      [Characteristic.RemoteKey.SELECT]: '40BF3D',
+      [Characteristic.RemoteKey.BACK]: '40BF3B',
+      [Characteristic.RemoteKey.EXIT]: '',
+      [Characteristic.RemoteKey.PLAY_PAUSE]: '40BE2D',
+      [Characteristic.RemoteKey.INFORMATION]: '',
+    }
+    const keyCode = map[key];
+    if (keyCode) {
+      await this._sendKey(keyCode);
+    } else {
+      this._log("Cannot send unsupported key: " + key);
+    }
+    callback();
+  }
 
   // async _getFeature() {
   //   this._log("Getting feature");
@@ -90,12 +137,12 @@ class RegzaAccessory {
     return contentType !== "other";
   }
 
-  // async _getMuteStatus() {
-  //   this._log("Getting mute status");
-  //   const res = await this._sendRequest(`https://${this.host}:4430`, "/v2/remote/status/mute");
-  //   const { status, mute } = res.data;
-  //   return status === 0 && mute === "on";
-  // }
+  async _getMuteStatus() {
+    this._log("Getting mute status");
+    const res = await this._sendRequest(`https://${this.host}:4430`, "/v2/remote/status/mute");
+    const { status, mute } = res.data;
+    return status === 0 && mute === "on";
+  }
 
   _md5sum(str) {
     return crypto.createHash('md5').update(str, 'binary').digest('hex');
